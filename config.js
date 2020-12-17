97d8ff4403c74d6db0952b80dd79248ec35a4cc9
Joe Karlsson <joekarlsson1@gmail.com>
2020-12-17 12:58:40 -0600

    commit.

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    connectionString: "mongodb+srv://joe:karlsson@cluster0.m7f10.mongodb.net/medicalRecords?retryWrites=true&w=majority",
-    dataKey: "HZrq+a+TSBeZ0txrrnhaqw==",
-}
